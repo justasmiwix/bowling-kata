e5fa35b0d4ae0e4bb36fcf4497fbb1cd5ab940f9
Andrius Butkevicius <andriusbu@wix.com>
2022-09-13 09:29:15 +0300

    Add example test case

--- src/test/scala/GameTest.scala
+++ src/test/scala/GameTest.scala
@@ -1,5 +1,16 @@
 package com.kata.bowling
 
 import org.specs2.mutable.SpecWithJUnit
+import org.specs2.specification.Scope
 
-class GameTest extends SpecWithJUnit {}
+class GameTest extends SpecWithJUnit {
+  "Bowling Game" should {
+    "score all 9 throws" in new ctx {
+      val game = new Game
+
+      game.score must_== 90
+    }
+  }
+
+  trait ctx extends Scope
+}
