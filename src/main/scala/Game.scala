f6df17ecb82743c0a72031990a9eea49d7da4ce3
Andrius Butkevicius <andriusbu@wix.com>
2022-09-29 09:49:21 +0300

    Minor improvements

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,5 +1,10 @@
 package com.kata.bowling
 
+/**
+ * Bowling Game class
+ *
+ * You can change anything here. These two methods are just an example.
+ */
 class Game {
   def roll(pinsDown: Int): Unit = ???
 
