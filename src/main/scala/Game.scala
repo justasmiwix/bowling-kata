cbc7c818ef54a11d507b6a1d84aff615591cf0de
Justas <justasmi@wix.com>
2023-07-04 15:14:02 +0300

    initial publish

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -5,8 +5,103 @@ package com.kata.bowling
  *
  * You can change anything here. These two methods are just an example.
  */
-class Game {
-  def roll(pinsDown: Int): Unit = ???
+class Game(rolls: Seq[Roll]) {
 
-  def score: Int = ???
+  private val (frames, (extraRoll1, extraRoll2)): (Seq[Frame], (Option[Roll], Option[Roll])) = calculateFrames
+  def roll(pinsDown: Int): Game = {
+    new Game(rolls :+ Roll(pinsDown))
+  }
+
+  def score: Int = {
+    val (mainFramesScore, _, _, _) = frames.foldLeft(0, false, false, false) {
+      case ((score, previousFrameWasSpare, previousPreviousFrameWasStrike, previousFrameWasStrike), currentFrame) => {
+        val newPreviousPreviousFrameWasStrike = if(currentFrame.isStrike) previousFrameWasStrike else false
+        val newPreviousFrameWasStrike = currentFrame.isStrike
+
+        val currentFrameScore = {
+          if(previousPreviousFrameWasStrike || previousFrameWasStrike)
+            currentFrame.scoreWithStrikeBonus(previousPreviousFrameWasStrike, previousFrameWasStrike)
+          else if (previousFrameWasSpare) currentFrame.scoreWithSpareBonus
+          else currentFrame.score
+        }
+        val newScore = score + currentFrameScore
+
+        (newScore, currentFrame.isSpare, newPreviousPreviousFrameWasStrike, newPreviousFrameWasStrike)
+      }
+    }
+
+    val extraRollsScore = extraRoll1.getOrElse(Roll(0)).score + extraRoll2.getOrElse(Roll(0)).score
+
+    println(mainFramesScore + " + " + extraRollsScore)
+    mainFramesScore + extraRollsScore
+  }
+
+  private def calculateFrames: (Seq[Frame], (Option[Roll], Option[Roll])) = {
+    val (previousValue, list) = rolls.foldLeft((0, Seq()): (Int, Seq[Frame])) { case ((previousValue, list), Roll(value)) =>
+      if(list.length == 10) 0 -> list
+      else {
+        if (previousValue == 0) { // first roll of the frame
+          if (value == 10) 0 -> (list :+ Frame(Roll(value), None))
+          else value -> list
+        }
+        else 0 -> (list :+ Frame(Roll(previousValue), Some(Roll(value))))
+      }
+    }
+
+    val paddedList =
+      if(previousValue != 0 && list.length < 10) list :+ Frame(Roll(previousValue), None)
+      else list
+
+    val extraRollsNeeded =
+      if(paddedList.length == 10) {
+        if(paddedList.last.isSpare) 1
+        else if(paddedList.last.isStrike) 2
+        else 0
+      }
+      else 0
+
+    val rollsInMainFrames = rollCountInFrames(paddedList)
+
+    val extraRolls = rolls.drop(rollsInMainFrames)
+
+    if(extraRolls.length > extraRollsNeeded) {
+      if(extraRollsNeeded == 0) throw new IllegalArgumentException("Cannot play over 10 frames")
+      else throw new IllegalArgumentException("Invalid amount of rolls")
+    }
+
+    val (extraRoll1, extraRoll2): (Option[Roll], Option[Roll]) = {
+      if(extraRollsNeeded == 0) (None, None)
+      else if(extraRollsNeeded == 1) (Some(nthSeqElementOption(extraRolls, 0, Roll(0))), None)
+      else (Some(nthSeqElementOption(extraRolls, 0, Roll(0))), Some(nthSeqElementOption(extraRolls, 1, Roll(0))))
+    }
+
+    (paddedList, (extraRoll1, extraRoll2))
+  }
+
+  private def nthSeqElementOption[A](list : Seq[A], index: Int, defaultVal: A) = {
+    val firstNElements = list.take(index + 1)
+    val nthElementOption = firstNElements.reverse.headOption.getOrElse(defaultVal)
+    nthElementOption
+  }
+  private def rollCountInFrames(frames: Seq[Frame]): Int =
+    frames.foldLeft(0)((acc, frame) => {
+      if (frame.isStrike) acc + 1
+      else acc + 2
+    })
+}
+case class Roll(pinsDown: Int) {
+  def score = pinsDown
+}
+
+case class Frame(roll1: Roll, roll2: Option[Roll]) {
+  def score = roll1.score + roll2.getOrElse(Roll(0)).score
+  def scoreWithSpareBonus = roll1.score * 2 + roll2.getOrElse(Roll(0)).score
+  def scoreWithStrikeBonus(previousPreviousFrameWasStrike: Boolean, previousFrameWasStrike: Boolean): Int = {
+    val roll1WithStrikeBonus = (Seq(previousPreviousFrameWasStrike, previousFrameWasStrike).filter(a => a == true).length + 1) * roll1.score
+    val roll2WithStrikeBonus = if(previousFrameWasStrike) roll2.getOrElse(Roll(0)).score * 2 else roll2.getOrElse(Roll(0)).score
+    roll1WithStrikeBonus + roll2WithStrikeBonus
+  }
+
+  def isSpare: Boolean = roll1.score + roll2.getOrElse(Roll(0)).score == 10 && roll1.score != 10
+  def isStrike: Boolean = roll1.score == 10
 }
--- src/test/scala/GameTest.scala
+++ src/test/scala/GameTest.scala
@@ -5,12 +5,72 @@ import org.specs2.specification.Scope
 
 class GameTest extends SpecWithJUnit {
   "Bowling Game" should {
-    "score all 9 throws" in new ctx {
-      val game = new Game
-
-      game.score must_== 90
+    /*"score one roll" in new ctx {
+      val score = 6
+      game.roll(score).score mustEqual score
     }
+
+    "score two rolls" in new ctx {
+      val roll1 = 2
+      val roll2 = 3
+
+      game.roll(roll1).roll(roll2).score mustEqual roll1 + roll2
+    }
+
+    /*"fail to play over 10 frames" in new ctx {
+      Seq.fill(20)(1).foldLeft(game)((game, roll) => {
+        game.roll(roll)
+      })
+        .roll(1) must throwA[IllegalArgumentException]("Cannot play over 10 frames")
+    }*/
+
+    "fail to play over 10 frames when last frame is not spare or strike" in new ctx {
+      Seq.fill(20)(1).foldLeft(game)((game, roll) => {
+        game.roll(roll)
+      })
+        .roll(1) must throwA[IllegalArgumentException]("Cannot play over 10 frames")
+    }
+
+    "score spare" in new ctx {
+      game.roll(8).roll(2).roll(6)
+        .score mustEqual 8 + 2 + 6 * 2
+    }
+
+    "score strike" in new ctx {
+      game.roll(10).roll(2).roll(3)
+        .score mustEqual 10 + (2 + 3) + (2 + 3)
+    }
+
+    "score 2 strikes in a row" in new ctx {
+      game.roll(10).roll(10).roll(2).roll(3)
+        .score mustEqual 10 + 10 + 10 + 2 + 2 + 2 + 3 + 3
+    }
+
+    "score 1 extra roll" in new ctx {
+      Seq.fill(18)(1).foldLeft(game)((game, roll) => {
+        game.roll(roll)
+      })
+        .roll(5).roll(5).roll(5).score mustEqual 18 + 5 + 5 + 5
+    }
+
+    "score 2 extra rolls" in new ctx {
+      Seq.fill(18)(1).foldLeft(game)((game, roll) => {
+        game.roll(roll)
+      })
+        .roll(10).roll(5).roll(5).score mustEqual 18 + 10 + 5 + 5
+    }*/
+
+    "score 12 strikes" in new ctx {
+      Seq.fill(12)(10).foldLeft(game)((game, roll) => {
+        game.roll(roll)
+      })
+        .score mustEqual 300
+    }
+
   }
 
-  trait ctx extends Scope
+
+  trait ctx extends Scope {
+    val game = new Game(Seq())
+  }
 }
